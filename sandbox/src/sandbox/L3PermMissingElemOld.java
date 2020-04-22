14285c4681742e842c0f050bf92bed414cf799b9
basha <basha@bashainfotech.com>
2020-04-22 18:47:24 -0400

    HackerRank-MinimumAbsoluteDifferenceInArray

--- sandbox/src/sandbox/L3PermMissingElemOld.java
+++ sandbox/src/sandbox/L3PermMissingElemOld.java
@@ -2,6 +2,7 @@ package sandbox;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -11,10 +12,10 @@ public class L3PermMissingElemOld {
 		int[] A = { 1, 2, 4 };
 		System.out.println("Solution 1:");
 		solution(A);
-		
+
 		System.out.println("Solution 2:");
 		solution2(A);
-		
+
 	}
 
 	public static int solution(int[] A) {
@@ -25,15 +26,14 @@ public class L3PermMissingElemOld {
 	}
 
 	public static int solution2(int[] A) {
-		var mySetA = IntStream.of(A).boxed().collect(Collectors.toSet());
-		//var mySetA = Set.of(Arrays.asList(A));
-		var mySetB = IntStream.rangeClosed(1, A.length).boxed().collect(Collectors.toSet());
+		Set<Integer> mySetA = IntStream.of(A).boxed().collect(Collectors.toSet());
+		// var mySetA = Set.of(Arrays.asList(A));
+		Set<Integer> mySetB = IntStream.rangeClosed(1, A.length).boxed().collect(Collectors.toSet());
 		mySetB.removeAll(mySetA);
-		
+
 		System.out.println("mySetA:" + mySetA);
 
 		mySetB.forEach(x -> System.out.println(x));
 		return -1;
 	}
 }
-
